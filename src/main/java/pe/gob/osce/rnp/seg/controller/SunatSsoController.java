c503f9fcc9a8d184ac7f7cb67f59c794e1f74adb
PC <peterpaul.0194@gmail.com>
2019-11-14 15:09:07 -0500

    refactorization

--- src/main/java/pe/gob/osce/rnp/seg/controller/SunatSsoController.java
+++ src/main/java/pe/gob/osce/rnp/seg/controller/SunatSsoController.java
@@ -10,11 +10,15 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+import pe.gob.osce.rnp.seg.cfg.SSLClientFactory;
 import pe.gob.osce.rnp.seg.model.dto.TokenDTO;
+import pe.gob.osce.rnp.seg.utils.Parseador;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 import java.util.Optional;
 
 @Controller
@@ -52,10 +56,13 @@ public class SunatSsoController {
     private String oauthSunatEndpointToken;
 
     @GetMapping("")
-    public @ResponseBody String intercambiarTokenPostLoginEnOauthSunat(@RequestParam(value = "code") String codigoParaCanjear){
+    public ModelAndView intercambiarTokenPostLoginEnOauthSunat(@RequestParam(value = "code") String codigoParaCanjear, HttpServletRequest request){
+        LOGGER.info("intercambiarTokenPostLoginEnOauthSunat(...) method called.");
+        ModelAndView model = new ModelAndView(VIEW_FINAL_SUNAT_SSO);
+
         String endpointGetTokenFromSunat = String.format(oauthSunatEndpointToken, oauthSunatClientId);
 
-        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate(SSLClientFactory.getClientHttpRequestFactory(SSLClientFactory.HttpClientType.HttpClient));
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
@@ -76,7 +83,15 @@ public class SunatSsoController {
                 LOGGER.info(secResponseEntity.getBody().toString());
                 Optional<TokenDTO> optTokenDto = Optional.ofNullable(secResponseEntity.getBody());
                 if(optTokenDto.isPresent()){
-                    return optTokenDto.get().getAccess_token();
+                    TokenDTO objToken = optTokenDto.get();
+                    //Encodificando
+                    Long time = new Date().getTime();
+                    String timeHash = Parseador.getEncodeHashLong32Id(SCHEMA_HASH, time);
+                    model.addObject("itsemitxam", timeHash);
+                    model.addObject("itslru", angularEndpoint);
+                    model.addObject("itspi", Parseador.encodeIpAddress(request.getRemoteAddr()));
+                    model.addObject("itsnekottanus", objToken.getAccess_token());
+                    return model;
                 }
             }
         }catch (HttpClientErrorException ex){
@@ -86,116 +101,6 @@ public class SunatSsoController {
             ex.printStackTrace();
             LOGGER.info(ex.getLocalizedMessage());
         }
-        return "Failed...check the console";
-    }
-
-
-    /*
-
-    @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-        LOGGER.info("SUNAT SSO CONTROLLER: handleRequestInternal*******************************");
-        ModelAndView model = new ModelAndView(VIEW_FINAL_SUNAT_SSO);
-        String rucAutenticado = obtenerRucDecodifcado();
-        if(rucAutenticado.equals("0"))
-           return model;
-        String oauthToken = obtenerToken();
-        if(oauthToken.equals("0"))
-           return model;
-        //Borrando el objeto en session
-        session.removeAttribute("usuarioBean");
-
-        //Invocando el endpoint para registrar ruc que va actualizar su correo(Como método de validación y respaldo de seguridad)
-        //registrarFlagPreviaActualizacionCorreo(rucAutenticado, httpServletRequest.getRemoteAddr(), oauthToken);
-        //Encodificando
-        Long time = new Date().getTime();
-        String rucHash = Parseador.getEncodeHashLong32Id(SCHEMA_HASH, Long.valueOf(rucAutenticado));
-        String timeHash = Parseador.getEncodeHashLong32Id(SCHEMA_HASH, time);
-        model.addObject("itscur", rucHash);
-        model.addObject("itsemitxam", timeHash);
-        model.addObject("itslru", angularEndpoint);
-        model.addObject("itspi", new String(
-                Base64.getEncoder().encode(
-                        httpServletRequest.getRemoteAddr().getBytes()),
-                StandardCharsets.UTF_8).replaceAll("\\=", ""));
         return model;
     }
-
-    private String obtenerRucDecodifcado(){
-        Optional<Object> optUser = Optional.ofNullable(session.getAttribute("usuarioBean"));
-        if(!optUser.isPresent()){
-            return "0";
-        }
-        return ((UsuarioBean) optUser.get()).getNumRUC();
-    }
-
-    private String obtenerToken(){
-        String authVisa = oauthClientId + ":" + oauthSecret;
-        String endpointOauthToken = oauthEndpointToken;
-        byte[] encodedAuth = org.apache.commons.codec.binary.Base64.encodeBase64(authVisa.getBytes(StandardCharsets.US_ASCII));
-
-        String authHeader = "Basic " + new String( encodedAuth );
-
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.set("Authorization", authHeader);
-
-        MultiValueMap<String, String> map =
-                new LinkedMultiValueMap<>();
-        map.add("grant_type","client_credentials");
-
-        HttpEntity<MultiValueMap<String, String>> entity =
-                new HttpEntity<>(map, headers);
-
-        try {
-            ResponseEntity<TokenDTO> secResponseEntity = restTemplate.exchange(endpointOauthToken, HttpMethod.POST, entity, TokenDTO.class);
-            if(secResponseEntity.getStatusCode() == HttpStatus.OK){
-                LOGGER.info(secResponseEntity.getBody().toString());
-                System.out.println(secResponseEntity.getBody().toString());
-                Optional<String> optTokenDto = Optional.ofNullable(secResponseEntity.getBody().getAccess_token());
-                if(optTokenDto.isPresent()){
-                    return optTokenDto.get();
-                }
-            }
-        }catch (HttpClientErrorException ex){
-            LOGGER.info(ex.getLocalizedMessage());
-        } catch (Exception ex){
-            LOGGER.info(ex.getLocalizedMessage());
-        }
-        return "0";
-    }
-
-    private String registrarFlagPreviaActualizacionCorreo(String ruc, String ipCliente, String token){
-        String endpointPostSso = endpointPostSsoSuccess;
-
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.set("Authorization", "Bearer "+token);
-
-        MultiValueMap<String, String> map =
-                new LinkedMultiValueMap<>();
-        map.add("ipCliente", ipCliente);
-        map.add("ruc",ruc);
-
-        HttpEntity<MultiValueMap<String, String>> entity =
-                new HttpEntity<>(map, headers);
-
-        try {
-            ResponseEntity<Respuesta> secResponseEntity = restTemplate.exchange(endpointPostSso, HttpMethod.POST, entity, Respuesta.class);
-            if(secResponseEntity.getStatusCode() == HttpStatus.OK){
-                LOGGER.info(secResponseEntity.getBody().toString());
-                Optional<Respuesta> optTokenDto = Optional.ofNullable(secResponseEntity.getBody());
-                if(optTokenDto.isPresent()){
-                    return optTokenDto.get().getResponseCode()+"";
-                }
-            }
-        }catch (HttpClientErrorException ex){
-            LOGGER.info(ex.getLocalizedMessage());
-        } catch (Exception ex){
-            LOGGER.info(ex.getLocalizedMessage());
-        }
-        return "0";
-    }*/
 }
--- src/main/java/pe/gob/osce/rnp/seg/utils/Parseador.java
+++ src/main/java/pe/gob/osce/rnp/seg/utils/Parseador.java
@@ -2,6 +2,9 @@ package pe.gob.osce.rnp.seg.utils;
 
 import org.hashids.Hashids;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 public class Parseador {
 
     private Parseador(){}
@@ -9,5 +12,11 @@ public class Parseador {
     public static String getEncodeHashLong32Id(String schema,  Long num){
         Hashids rfIdHash = new Hashids(schema, 32);
         return rfIdHash.encode(num);
+    }
+
+    public static String encodeIpAddress(String remoteAddress){
+        return new String(
+                Base64.getEncoder().encode(remoteAddress.getBytes()),
+                StandardCharsets.UTF_8).replaceAll("\\=", "");
     }
 }
